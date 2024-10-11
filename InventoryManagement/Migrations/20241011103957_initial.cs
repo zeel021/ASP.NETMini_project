eed5a4abafe29c302e34df650fcebb5aca170462
zeel021 <145215116+zeel021@users.noreply.github.com>
2024-10-11 16:16:35 +0530

    Initial commit

---
+++ InventoryManagement/Migrations/20241011103957_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryManagement.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    Category = table.Column<string>(unicode: false, maxLength: 30, nullable: true),
+                    Color = table.Column<string>(unicode: false, maxLength: 15, nullable: true),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18, 0)", nullable: false),
+                    AvailableQuantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Products__B40CC6CD73B27013", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
